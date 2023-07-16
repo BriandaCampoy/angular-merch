1a6066a128dd7be23ba580cbf66fd928238a8fc9
BriandaCampoy <brianda.campoy@gmail.com>
2023-07-15 20:55:01 -0600

    nav selectors

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { OrdersService } from 'src/app/services/orders.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -8,6 +9,20 @@ import { OrdersService } from 'src/app/services/orders.service';
 })
 export class NavComponent {
 ordersService: OrdersService = inject(OrdersService);
+router: Router = inject(Router);
+selectedRoute = '';
+
+ngOnInit() {
+  this.router.events.subscribe(event => {
+    if (event instanceof NavigationEnd) {
+      console.log(event);
+      const params = event.url;
+      this.selectedRoute = params==='/'?'':params;
+
+      // Aquí puedes ejecutar la función que deseas cada vez que se actualice la ruta
+    }
+  });
+}
 
 shoppingCartCounter(){
   return this.ordersService?.currentOrder.length <= 9 ? this.ordersService.currentOrder.length : '+9';
